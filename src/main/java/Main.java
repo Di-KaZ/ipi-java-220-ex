3bc43cbecda15628337c6c330320836312498f8a
GET_MOUSSED <33959887+Di-KaZ@users.noreply.github.com>
2020-10-07 21:22:21 +0200

    added ex till 207

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
-import com.ipiecoles.java.java220.Employe;
+import com.ipiecoles.java.java220.Commercial;
 
 import org.joda.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Employe test = new Employe("toto", "tata", "titi", LocalDate.now(), 203.5);
-        Employe test2 = new Employe("toto", "tata", "titi", test.getDateEmbauche(), 203.5);
+        Commercial test = new Commercial("toto", "tata", "titi", LocalDate.now(), 203.5, 25000.0);
+        Commercial test2 = new Commercial("toto", "tata", "titi", test.getDateEmbauche(), 203.5, 25000.0);
         System.out.println(test.equals(test));
         System.out.println(test.equals(test2));
     }
--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,7 +1,15 @@
 package com.ipiecoles.java.java220;
+import org.joda.time.LocalDate;
 
 public class Commercial extends Employe {
     private Double caAnnuel;
+    private Integer performance;
+
+    public boolean performanceEgale(Integer performance) {
+        if (performance == null)
+            return false;
+        return this.performance.equals(performance);
+    }
 
     public Double getCaAnnuel() {
         return this.caAnnuel;
@@ -21,5 +29,45 @@ public class Commercial extends Employe {
         else
             return res;
     }
+    public Commercial() {
 
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
+        super(nom , prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel, Integer performance) {
+        super(nom , prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+        this.performance = performance;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null ||
+        this.getClass() != other.getClass() ||
+        !super.equals(other) ||
+        !this.caAnnuel.equals(((Commercial)other).getCaAnnuel()))
+        return false;
+        return true;
+    }
+
+    public Note equivalenceNote(Integer note) {
+        switch (note) {
+            case 0:
+                return Note.INSUFFISANT;
+            case 50:
+                return Note.INSUFFISANT;
+            case 100:
+                return Note.PASSABLE;
+            case 150:
+                return Note.BIEN;
+            case 200:
+                return Note.TRES_BIEN;
+            default:
+                return null;
+        }
+    }
 }
